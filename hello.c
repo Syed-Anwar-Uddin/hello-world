30e47cf9613a253fc98e3c8d7a3ec7f9671478eb
Syed Anwaruddin <anwars.be26@uceou.edu>
2025-03-15 18:16:55 +0530

    maininging

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@ int main()
     printf("Hello World\n");
     int a = 10;
     int b = 20;
-    printf("Sum of a and b is %d\n", a+b);
+    printf("Sum of the numbers a and b is %d\n", a+b);
+    printf("Difference of the numbers a and b is %d\n", a-b);
     return 0;
 }
